4dbc24b7565aba44c88bd0bc873152ec89388929
sashida <sashida@primagest.co.jp>
2020-10-06 08:36:01 +0900

    p.403

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 import { GeneralList } from "./GeneralList";
 import { SortedList } from "./SortedList";
-// import { ProFeature } from "./ProFeature";
-import { ProController } from "./ProController";
-import { LogToConsole } from "./LogToConsole";
-
-// const ProList = ProFeature(SortedList);
-// const ProList = ProController(SortedList);
-const ProList = ProController(LogToConsole(SortedList, "Sorted", true, true, true));
+import { ProFeature } from "./ProFeature";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       names: [ "Zoe", "Bob", "Alice", "Dora", "Joe" ],
-      cities: [ "London", "New York", "Paris", "Milan", "Boston" ]
+      cities: [ "London", "New York", "Paris", "Milan", "Boston" ],
+      proMode: false
     };
   }
 
@@ -24,17 +19,19 @@ export default class App extends Component {
   render() {
     return <div className="container-fluid">
       <div className="row">
-        <div className="col-3">
+        <div className="col-12 text-center p-2">
+          <div className="form-check">
+            <input type="checkbox" className="form-check-input" value={ this.state.proMode } onChange={ this.toggleProMode } />
+            <label className="form-check-label">Pro Mode</label>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
           <GeneralList list={ this.state.names } theme="primary" />
         </div>
-        <div className="col-3">
-          <ProList list={ this.state.names } />
-        </div>
-        <div className="col-3">
-          <GeneralList list={ this.state.cities } theme="secondary" />
-        </div>
-        <div className="col-3">
-          <ProList list={ this.state.cities } />
+        <div className="col-6">
+          <ProFeature pro={ this.state.proMode } render={ () => <SortedList list={ this.state.names } /> } />
         </div>
       </div>
     </div>
