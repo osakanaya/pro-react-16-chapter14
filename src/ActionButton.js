4435206ab69eb4c322d2445ac05e96b970f2f21f
sashida <sashida@primagest.co.jp>
2020-10-06 08:59:04 +0900

    p.412

--- src/ActionButton.js
+++ src/ActionButton.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import { ProModeContext } from "./ProModeContext";
 
 export class ActionButton extends Component {
     render() {
         return (
-            <button className={ this.getClasses(this.props.proMode) }
-                disabled={ !this.props.proMode } 
-                onClick={ this.props.callback }>
-                { this.props.text }
-            </button>
+            <ProModeContext.Consumer>
+                { contextData => 
+                    <button className={ this.getClasses(contextData.proMode) }
+                        disabled={ !contextData.proMode } 
+                        onClick={ this.props.callback }>
+                        { this.props.text }
+                    </button>
+                }
+            </ProModeContext.Consumer>
         );
     }
 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { GeneralList } from "./GeneralList";
 import { SortedList } from "./SortedList";
+import { ProModeContext } from "./ProModeContext";
 
 export default class App extends Component {
   constructor(props) {
@@ -8,19 +9,27 @@ export default class App extends Component {
     this.state = {
       names: [ "Zoe", "Bob", "Alice", "Dora", "Joe" ],
       cities: [ "London", "New York", "Paris", "Milan", "Boston" ],
-      proMode: false
+      proContextData: {
+        proMode: false
+      }
     };
   }
 
   toggleProMode = () => {
-    this.setState({ proMode: !this.state.proMode });
+    if (this.state.proContextData.proMode) {
+      this.setState(state => state.proContextData.proMode = false);
+    } else {
+      this.setState(state => state.proContextData.proMode = true);
+    }
   }
+
   render() {
     return <div className="container-fluid">
       <div className="row">
         <div className="col-12 text-center p-2">
           <div className="form-check">
-            <input type="checkbox" className="form-check-input" value={ this.state.proMode } onChange={ this.toggleProMode } />
+            <input type="checkbox" className="form-check-input" 
+              value={ this.state.proContextData.proMode } onChange={ this.toggleProMode } />
             <label className="form-check-label">Pro Mode</label>
           </div>
         </div>
@@ -30,7 +39,9 @@ export default class App extends Component {
           <GeneralList list={ this.state.names } theme="primary" />
         </div>
         <div className="col-6">
-          <SortedList proMode={ this.state.proMode } list={ this.state.names } />
+          <ProModeContext.Provider value={ this.state.proContextData }>
+            <SortedList list={ this.state.names } />
+          </ProModeContext.Provider>
         </div>
       </div>
     </div>
