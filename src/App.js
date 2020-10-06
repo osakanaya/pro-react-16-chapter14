2b6d71af376729b5e6a57cac2fb328117881622d
sashida <sashida@primagest.co.jp>
2020-10-06 09:04:58 +0900

    p.414

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { GeneralList } from "./GeneralList";
 import { SortedList } from "./SortedList";
 import { ProModeContext } from "./ProModeContext";
+import { ProModeToggle } from "./ProModeToggle";
+
 
 export default class App extends Component {
   constructor(props) {
@@ -10,7 +12,8 @@ export default class App extends Component {
       names: [ "Zoe", "Bob", "Alice", "Dora", "Joe" ],
       cities: [ "London", "New York", "Paris", "Milan", "Boston" ],
       proContextData: {
-        proMode: false
+        proMode: false,
+        toggleProMode: this.toggleProMode
       }
     };
   }
@@ -25,25 +28,21 @@ export default class App extends Component {
 
   render() {
     return <div className="container-fluid">
-      <div className="row">
-        <div className="col-12 text-center p-2">
-          <div className="form-check">
-            <input type="checkbox" className="form-check-input" 
-              value={ this.state.proContextData.proMode } onChange={ this.toggleProMode } />
-            <label className="form-check-label">Pro Mode</label>
+      <ProModeContext.Provider value={ this.state.proContextData }>
+        <div className="row">
+          <div className="col-12 text-center p-2">
+            <ProModeToggle label="Pro Mode" />
           </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <GeneralList list={ this.state.names } theme="primary" />
+        <div className="row">
+          <div className="col-6">
+            <GeneralList list={ this.state.names } theme="primary" />
+          </div>
+          <div className="col-6">
+              <SortedList list={ this.state.names } />
+          </div>
         </div>
-        <div className="col-6">
-          <ProModeContext.Provider value={ this.state.proContextData }>
-            <SortedList list={ this.state.names } />
-          </ProModeContext.Provider>
-        </div>
-      </div>
+      </ProModeContext.Provider>
     </div>
   }
 }
--- src/ProModeContext.js
+++ src/ProModeContext.js
@@ -1,5 +1,6 @@
 import React from "react";
 
 export const ProModeContext = React.createContext({
-    proMode: false
+    proMode: false,
+    toggleProMode: () => {}
 });
