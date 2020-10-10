47b013b1b62351d100f131caf0b8a47b8168e658
sashida <sashida@primagest.co.jp>
2020-10-10 09:52:38 +0900

    p.422

--- src/SortedList.js
+++ src/SortedList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { GeneralList } from "./GeneralList";
 import { ActionButton } from "./ActionButton";
+import { ErrorBoundary } from "./ErrorBoundary";
 
 export class SortedList extends Component {
     constructor(props) {
@@ -21,10 +22,12 @@ export class SortedList extends Component {
     render() {
         return (
             <div>
-                <GeneralList list={ this.getList() } theme="info" />
-                <div className="text-center m-2">
-                    <ActionButton theme="primary" text="Sort" proMode={ this.props.proMode } callback={ this.toggleSort } />
-                </div>
+                <ErrorBoundary>
+                    <GeneralList list={ this.getList() } theme="info" />
+                    <div className="text-center m-2">
+                        <ActionButton theme="primary" text="Sort" proMode={ this.props.proMode } callback={ this.toggleSort } />
+                    </div>
+                </ErrorBoundary>
             </div>
         )
     }
