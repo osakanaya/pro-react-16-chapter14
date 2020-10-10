47f7f341329fede38ce8e13b7ff033792c4a3394
sashida <sashida@primagest.co.jp>
2020-10-10 09:41:12 +0900

    p.418

--- src/ProModeToggle.js
+++ src/ProModeToggle.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ProModeContext } from "./ProModeContext";
 
-export class ProModeToggle extends Component {
-    render() {
-        return <ProModeContext.Consumer>
-            { contextData => (
-                <div className="form-check">
-                    <input type="checkbox" className="form-check-input"
-                        value={ contextData.proMode } onChange={ contextData.toggleProMode } />
-                    <label className="form-check-label">
-                        { this.props.label }
-                    </label>
-                </div>
-            )}
-        </ProModeContext.Consumer>
-    }
+export function ProModeToggle(props) {
+    const context = useContext(ProModeContext);
+
+    return (
+        <div className="form-check">
+            <input type="checkbox" className="form-check-input"
+                value={ context.proMode } onChange={ context.toggleProMode } />
+            <label className="form-check-label">
+                { props.label }
+            </label>
+        </div>
+        );
 }
