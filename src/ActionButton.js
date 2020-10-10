6872a2bcfe58e045481af23141b408b695748de2
sashida <sashida@primagest.co.jp>
2020-10-10 09:45:15 +0900

    p.419

--- src/ActionButton.js
+++ src/ActionButton.js
@@ -2,16 +2,32 @@ import React, { Component } from "react";
 import { ProModeContext } from "./ProModeContext";
 
 export class ActionButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clickCount: 0
+        };
+    }
+
+    handleClick = () => {
+        this.setState({ clickCount: this.state.clickCount + 1 });
+        this.props.callback();
+    }
+
     render() {
         return (
             <ProModeContext.Consumer>
-                { contextData => 
-                    <button className={ this.getClasses(contextData.proMode) }
+                { contextData => {
+                    if (this.state.clickCount > 1) {
+                        throw new Error("Click Counter Error");
+                    } 
+
+                    return <button className={ this.getClasses(contextData.proMode) }
                         disabled={ !contextData.proMode } 
-                        onClick={ this.props.callback }>
+                        onClick={ this.handleClick }>
                         { this.props.text }
                     </button>
-                }
+                }}
             </ProModeContext.Consumer>
         );
     }
